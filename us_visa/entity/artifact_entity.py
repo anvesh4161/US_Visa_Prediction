71df0ec4ee5d73c558497dc219ece7d5d55f926a
anvesh4161 <anveshbunty01@gmail.com>
2024-05-16 06:35:24 +0530

    Model Trainer component completed

--- us_visa/entity/artifact_entity.py
+++ us_visa/entity/artifact_entity.py
@@ -26,3 +26,16 @@ class ClassificationMetricArtifact:
     f1_score:float
     precision_score:float
     recall_score:float
+
+
+@dataclass
+class ClassificationMetricArtifact:
+    f1_score:float
+    precision_score:float
+    recall_score:float
+
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path:str 
+    metric_artifact:ClassificationMetricArtifact
